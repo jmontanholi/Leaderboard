652954757b277f4743dafb916940f5749978aa8d
jmontanholi <jonunes9326@hotmail.com.br>
2021-07-30 11:18:29 -0300

    Fix submit margin when invalid submit

--- src/AddScores.js
+++ src/AddScores.js
@@ -1,13 +1,13 @@
 import { createScores } from './request.js';
 
-const error = document.getElementById('error');
-const submitBtn = document.getElementById('submitBtn');
 const userName = document.getElementById('userName');
 const userScore = document.getElementById('userScore');
 
 const addScore = () => {
+  const error = document.getElementById('error');
   const userName = document.getElementById('userName');
   const userScore = document.getElementById('userScore');
+  const submitBtn = document.getElementById('submitBtn');
 
   if (userName.value.trim() === '' || userScore.value.trim() === '') {
     error.classList.remove('d-none');
@@ -15,10 +15,15 @@ const addScore = () => {
     submitBtn.classList.remove('submit');
   } else {
     createScores(userName.value, Number(userScore.value));
+    error.classList.add('d-none');
+    error.classList.remove('d-grid');
+    submitBtn.classList.add('submit');
   }
 };
 
 const removeError = () => {
+  const error = document.getElementById('error');
+  const submitBtn = document.getElementById('submitBtn');
   userName.addEventListener('change', () => {
     error.classList.remove('d-grid');
     error.classList.add('d-none');
